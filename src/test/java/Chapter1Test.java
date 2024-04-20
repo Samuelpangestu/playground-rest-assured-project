c341064ca500a63fbc06cc5ad6ef1e7a27d953aa
Samuel Pangestu G Tobing <samuel.pangestu.gonggom@gmail.com>
2024-04-20 20:46:02 +0700

    add more scenario

--- src/test/java/Chapter1Test.java
+++ src/test/java/Chapter1Test.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
@@ -5,10 +6,50 @@ import static org.hamcrest.Matchers.*;
 
 public class Chapter1Test {
     @Test
-    public void request_check_expect() {
+    public void validatePlaceName() {
         given().
-        when().get("http://api.zippopotam.us/us/90210").
-        then().assertThat().body("places[0].'place name'",equalTo("Beverly Hills"));
+                log().all().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+
+                then().
+                assertThat().
+                body("places[0].'place name'", equalTo("Beverly Hills")).
+                statusCode(200).
+                contentType(ContentType.JSON).
+                log().body();
     }
 
+    @Test
+    public void validatePlaceState() {
+        given().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+
+                then().
+                assertThat().
+                body("places[0].state", equalTo("California"));
+    }
+
+    @Test
+    public void validatePlaceNameContains() {
+        given().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+
+                then().
+                assertThat().
+                body("places.'place name'",hasItem("Beverly Hills"));
+    }
+
+    @Test
+    public void validatePlaceNameSize() {
+        given().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+
+                then().
+                assertThat().
+                body("places.'place name'", hasSize(1));
+    }
 }
